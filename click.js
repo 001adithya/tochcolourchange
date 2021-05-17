e54595aa20ffbec186d4f67c54201cc54839ddce
001adithya <84325705+001adithya@users.noreply.github.com>
2021-05-17 21:37:25 +0530

    Add files via upload

---
+++ click.js
@@ -0,0 +1,15 @@
+alert("eda monu njan seen ane")
+function red__cr(){
+    alert("red matio")
+ document.body.style.background="red"
+}
+
+function green__cr(){
+    alert("green ayalo")
+    document.body.style.background="green"
+}
+
+function blue__cr(){
+    alert("dark ano")
+    document.body.style.background="black" 
+}
